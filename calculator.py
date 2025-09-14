112f4cc84430723520b8f3c9f6cb5dfc2d0756a2
Khalid Ahmed Tepu <khalidahmedtepu@gmail.com>
2025-09-14 19:30:53 +0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+x = 7
+y = 3
+
+print("Addition:", x + y)
+print("Subtraction:", x - y)
+print("Multiplication:", x * y)
+print("Division:", x / y)
+print("Floor Division:",x//y)
+print("Modulus:",x%y)
